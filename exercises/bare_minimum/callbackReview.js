1542939c2c56d3c6f645b7a49a55c0d9e0feddc5
Jordan <jordanace83@gmail.com>
2021-02-23 17:24:12 -0600

    Complete bare_minimum/callbackReview.js exercises

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,14 +6,32 @@ var fs = require('fs');
 var request = require('needle');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
+var pluckFirstLineFromFile = function (filePath, callback) {
+  fs.readFile(filePath, 'utf8', function (err, data) {
+    if (err) {
+      callback(err);
+    } else {
+      var firstLine = data.split('\n')[0];
+      callback(err, firstLine);
+    }
+  });
   // TODO
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, callback) {
+  request('get', url)
+    .then(function(resp) { callback(null, resp.statusCode); })
+    .catch(callback);
 };
+
+// Calling needle() directly returns a Promise.
+// Besides method and url, all parameters are optional, although when sending a post, put or patch request you will get an error if data is not present.
+
+// needle('get', 'http://some.url.com')
+//   .then(function(resp) { console.log(resp.body) })
+//   .catch(function(err) { console.error(err) })
+// })
 
 // Export these functions so we can test them and reuse them in later exercises
 module.exports = {
